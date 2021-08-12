a3f589a86509fd97f67623d8655eaa96030193e9
Daniel Waraksa <danw8989@gmail.com>
2021-08-12 17:45:43 +0200

    first commit

---
+++ restaurant/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+class Dish(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    prepare_time = models.DurationField()
+    created_at = models.DateField(auto_now_add=True)
+    modified_at = models.DateField(auto_now=True)
+    is_vegetarian = models.BooleanField()
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
+    modified_at = models.DateField(auto_now=True)
+    dishes = models.ManyToManyField(Dish)
