41811505758ea92cbb0148884ed2adaaa3401b66
Daniel Waraksa <danw8989@gmail.com>
2021-08-13 10:41:34 +0200

    menu serializer

--- restaurant/serializers.py
+++ restaurant/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
+from models import *
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -12,3 +13,15 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ['url', 'name']
+
+
+class MenuSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Menu
+        fields = ['title',
+                  'description',
+                  'price',
+                  'prepare_time',
+                  'created_at',
+                  'modified_at',
+                  'is_vegetarian', ]
