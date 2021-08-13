245af918f969896a5771b37e1d6ca3506647a358
Daniel Waraksa <danw8989@gmail.com>
2021-08-13 14:41:48 +0200

    fix models

--- restaurant/models.py
+++ restaurant/models.py
@@ -8,14 +8,14 @@ class Dish(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     prepare_time = models.DurationField()
-    created_at = models.DateField(auto_now_add=True)
-    modified_at = models.DateField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
     is_vegetarian = models.BooleanField()
 
 
 class Menu(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, unique=True)
     description = models.TextField()
-    created_at = models.DateField(auto_now_add=True)
-    modified_at = models.DateField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
     dishes = models.ManyToManyField(Dish)
