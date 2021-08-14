8174d477182b436b8f1ae3eefe9ec71462bdedf8
Daniel Waraksa <danw8989@gmail.com>
2021-08-14 10:37:22 +0200

    redirect

--- storecard/urls.py
+++ storecard/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 import debug_toolbar
+from .views import index
 
 urlpatterns = [
+    path('', index),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('__debug__/', include(debug_toolbar.urls)),
