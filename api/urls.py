1c7635d0e7145c63c326d50b12d7615566099c5a
Daniel Waraksa <danw8989@gmail.com>
2021-08-23 18:26:56 +0200

    used another crud generics

--- api/urls.py
+++ api/urls.py
@@ -12,17 +12,14 @@ urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
-    path("dishes/",views.ListDishesAPIView.as_view(),name="dish_list"),
-    path("dishes/create/", views.CreateDishAPIView.as_view(),name="dish_create"),
-    path("dish/<int:pk>/",views.RetrieveDishAPIView.as_view(),name="retrieve_dish"),
-    path("dish/update/<int:pk>/",views.UpdateDishAPIView.as_view(),name="update_dish"),
-    path("dish/delete/<int:pk>/",views.DeleteDishAPIView.as_view(),name="delete_dish"),
+    path("dishes/", views.ListDishesAPIView.as_view(), name="dish_list"),
+    path("dishes/create/", views.CreateDishAPIView.as_view(), name="dish_create"),
+    path("dish/<int:pk>/", views.RetrieveUpdateDestroyDishAPIView.as_view(),
+         name="retrieve_dish"),
 
+    path("menus/", views.ListMenuAPIView.as_view(), name="menu_list"),
+    path("menus/create/", views.CreateMenuAPIView.as_view(), name="menu_create"),
+    path("menu/<int:pk>/", views.RetrieveUpdateDestroyMenuAPIView.as_view(),
+         name="retrieve_menu"),
 
-    path("menus/",views.ListMenuAPIView.as_view(),name="menu_list"),
-    path("menus/create/", views.CreateMenuAPIView.as_view(),name="menu_create"),
-    path("menu/<int:pk>/",views.RetrieveMenuAPIView.as_view(),name="retrieve_menu"),
-    path("menu/update/<int:pk>/",views.UpdateMenuAPIView.as_view(),name="update_menu"),
-    path("menu/delete/<int:pk>/",views.DeleteMenuAPIView.as_view(),name="delete_menu")
 ]
-
--- api/views.py
+++ api/views.py
@@ -1,17 +1,14 @@
 
+from api.perms import IsGetOrIsAuthenticated
 from api.filters import MenuFilter
 from django_filters import rest_framework as filters
 from rest_framework import generics
 from api.models import Dish, Menu
-from django.http import Http404
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets, permissions, status
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework.generics import CreateAPIView, DestroyAPIView, ListAPIView, UpdateAPIView, RetrieveAPIView
+from django.contrib.auth.decorators import login_required
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveUpdateDestroyAPIView
 from api.serializers import DishSerializer, MenuDetailSerializer, MenuSerializer, UserSerializer, GroupSerializer
-from .perms import IsGetOrIsAuthenticated
-from django.db.models import Count
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -20,7 +17,7 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated, ]
 
 
 class GroupViewSet(viewsets.ModelViewSet):
@@ -29,7 +26,7 @@ class GroupViewSet(viewsets.ModelViewSet):
     """
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated, ]
 
 
 class ListDishesAPIView(ListAPIView):
@@ -38,48 +35,31 @@ class ListDishesAPIView(ListAPIView):
     """
     queryset = Dish.objects.all()
     serializer_class = DishSerializer
+    permissions_classes = [permissions.AllowAny]
 
 
 class CreateDishAPIView(CreateAPIView):
     """
     Creates a new dish
     """
-    permissions = [permissions.IsAuthenticated]
     queryset = Dish.objects.all()
     serializer_class = DishSerializer
 
 
-class UpdateDishAPIView(UpdateAPIView):
-    """
-    Updates dish instance with id provided
-    """
-    permissions = [permissions.IsAuthenticated]
+class RetrieveUpdateDestroyDishAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Dish.objects.all()
     serializer_class = DishSerializer
 
 
-class DeleteDishAPIView(DestroyAPIView):
-    """
-    Deletes dish instance with id provided
-    """
-    permissions = [permissions.IsAuthenticated]
-    queryset = Dish.objects.all()
-    serializer_class = DishSerializer
-
-
-class RetrieveDishAPIView(RetrieveAPIView):
-    """
-    Retrieves dish instance with id provided
-    """
-    permissions = [permissions.IsAuthenticated]
-    queryset = Dish.objects.all()
-    serializer_class = DishSerializer
-
+# ----------------------------------------------------------
+# MENUS
 
 class ListMenuAPIView(ListAPIView):
     """
     Lists all menus from the database\n
     """
+    permission_classes = [permissions.AllowAny]
+
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
     #filter_backends = (filters.DjangoFilterBackend,)
@@ -90,33 +70,15 @@ class CreateMenuAPIView(CreateAPIView):
     """
     Creates a new menu instance
     """
-    permissions = [permissions.IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
 
-class UpdateMenuAPIView(UpdateAPIView):
-    """
-    Updates menu instance with id provided
-    """
-    permissions = [permissions.IsAuthenticated]
+class RetrieveUpdateDestroyMenuAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+    permission_classes = [IsGetOrIsAuthenticated, ]
 
-
-class DeleteMenuAPIView(DestroyAPIView):
-    """
-    Deletes menu instance with id provided
-    """
-    permissions = [permissions.IsAuthenticated]
-    queryset = Menu.objects.all()
-    serializer_class = MenuSerializer
-
-
-class RetrieveMenuAPIView(RetrieveAPIView):
-    """
-    Retrieves menu instance (with detail) with id provided
-    """
-    permissions = [permissions.IsAuthenticated]
-    queryset = Menu.objects.all()
-    serializer_class = MenuDetailSerializer
+    def retrieve(self, request, *args, **kwargs):
+        self.serializer_class = MenuDetailSerializer
+        return super().retrieve(request, *args, **kwargs)
