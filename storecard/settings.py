41262c51e61d66543aa5c9f4521029bd561474e5
Daniel Waraksa <danw8989@gmail.com>
2021-08-23 18:29:20 +0200

    removed pagination

--- storecard/settings.py
+++ storecard/settings.py
@@ -46,8 +46,9 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
