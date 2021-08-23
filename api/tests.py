bdd0f99fc100642ec31dcd7bf9dfff902e172581
Daniel Waraksa <danw8989@gmail.com>
2021-08-23 19:10:27 +0200

    merged auth noauth test

--- api/tests.py
+++ api/tests.py
@@ -31,7 +31,7 @@ class DishTestCases(APITestCase):
                            price=32.56, prepare_time='00:50:30', is_vegetarian=True)
         sample_dish.save()
 
-    def test_post_dish_authenticated(self):
+    def test_post_dish(self):
         data = json.dumps({
             "title": "test",
             "description": "test_test",
@@ -39,70 +39,40 @@ class DishTestCases(APITestCase):
             "prepare_time": "00:30:00",
             "is_vegetarian": True,
         })
+        response = self.client.post(
+            '/api/dishes/create/', data, content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
         self.login()
         response = self.client.post(
             '/api/dishes/create/', data, content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-    def test_post_dish_unauthenticated(self):
-        data = json.dumps({
-            "title": "test2",
-            "description": "test_test",
-            "price": "43.99",
-            "prepare_time": "00:30:00",
-            "is_vegetarian": True,
-        })
-        response = self.client.post(
-            '/api/dishes/create/', data, content_type='application/json')
+    def test_get_dishes(self):
+        response = self.client.get('/api/dishes/')
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
-    def test_get_dishes_authenticated(self):
         self.login()
         response = self.client.get('/api/dishes/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_get_dishes_unauthenticated(self):
-        response = self.client.get('/api/dishes/')
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
-    def test_delete_dish_authenticated(self):
+    def test_delete_dish(self):
         self.add_test_dish()
+        response = self.client.delete('/api/dish/1/')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
         self.login()
         response = self.client.delete('/api/dish/1/')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
-    def test_delete_dish_unauthenticated(self):
+    def test_get_dish(self):
         self.add_test_dish()
-        response = self.client.delete('/api/dish/1/')
+        response = self.client.get('/api/dish/1/')
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
-    def test_get_dish_authenticated(self):
-        self.add_test_dish()
         self.login()
         response = self.client.get('/api/dish/1/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_get_dish_unauthenticated(self):
+    def test_update_dish(self):
         self.add_test_dish()
-        response = self.client.get('/api/dish/1/')
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
-    def test_update_dish_authenticated(self):
-        self.add_test_dish()
-        self.login()
-        data = json.dumps({
-            "title": "test2",
-            "description": "test_test",
-            "price": "43.99",
-            "prepare_time": "00:30:00",
-            "is_vegetarian": True,
-        })
-        response = self.client.put(
-            '/api/dish/1/', data, content_type='application/json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_update_dish_unauthenticated(self):
-        self.add_test_dish()
         data = json.dumps({
             "title": "test2",
             "description": "test_test",
@@ -113,6 +83,10 @@ class DishTestCases(APITestCase):
         response = self.client.put(
             '/api/dish/1/', data, content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.login()
+        response = self.client.put(
+            '/api/dish/1/', data, content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
 
 class MenuTestCase(APITestCase):
@@ -140,81 +114,53 @@ class MenuTestCase(APITestCase):
         sample_menu.dishes.add(sample_dish.id)
         sample_menu.save()
 
-    def test_post_menu_authenticated(self):
+    def test_post_menu(self):
 
         data = json.dumps({
             "title": "test2",
             "description": "test_test",
             "dishes": [1]
         })
-        self.login()
+
         sample_dish = Dish(id=1, title='test', description='testest',
                            price=32.56, prepare_time='00:50:30', is_vegetarian=True)
         sample_dish.save()
         self.assertEqual(Dish.objects.count(), 1)
+
+        response = self.client.post(
+            '/api/menus/create/', data, content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+        self.login()
+
         response = self.client.post(
             '/api/menus/create/', data, content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-    def test_post_menu_unauthenticated(self):
-
-        data = json.dumps({
-            "title": "test2",
-            "description": "test_test",
-            "dishes": [1]
-        })
-        sample_dish = Dish(id=1, title='test', description='testest',
-                           price=32.56, prepare_time='00:50:30', is_vegetarian=True)
-        sample_dish.save()
-        self.assertEqual(Dish.objects.count(), 1)
-        response = self.client.post(
-            '/api/menus/create/', data, content_type='application/json')
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
-    def test_get_menus_authenticated(self):
+    def test_get_menus(self):
+        response = self.client.get('/api/menus/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.login()
         response = self.client.get('/api/menus/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_get_menus_unauthenticated(self):
-        response = self.client.get('/api/menus/')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_delete_menu_authenticated(self):
+    def test_delete_menu(self):
         self.add_test_menu()
+        response = self.client.delete('/api/menu/1/')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
         self.login()
         response = self.client.delete('/api/menu/1/')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
-    def test_delete_menu_unauthenticated(self):
+    def test_get_menu(self):
         self.add_test_menu()
-        response = self.client.delete('/api/menu/1/')
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
-    def test_get_menu_authenticated(self):
-        self.add_test_menu()
+        response = self.client.get('/api/menu/1/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.login()
         response = self.client.get('/api/menu/1/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_get_menu_unauthenticated(self):
-        self.add_test_menu()
-        response = self.client.get('/api/menu/1/')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_update_menu_authenticated(self):
-        self.add_test_menu()
-        self.login()
-        data = json.dumps({
-            "title": "teswerwert2",
-            "description": "test_test",
-            "dishes": [1]
-        })
-        response = self.client.put(
-            '/api/menu/1/', data, content_type='application/json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_update_menu_unauthenticated(self):
+    def test_update_menu(self):
         self.add_test_menu()
         data = json.dumps({
             "title": "teswerwert2",
@@ -224,6 +170,12 @@ class MenuTestCase(APITestCase):
         response = self.client.put(
             '/api/menu/1/', data, content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.login()
+
+        response = self.client.put(
+            '/api/menu/1/', data, content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
 
     def test_search_title(self):
         self.add_test_menu()
